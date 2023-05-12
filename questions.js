83b548189026b78226a05c4d8abbc7a1235542c0
Eddie <Edward@veronicas-Air.attlocal.net>
2023-05-11 21:30:36 -0700

    coding files uploaded

---
+++ questions.js
@@ -0,0 +1,41 @@
+let questions = [
+    {   num: 1,
+        question: 'Arays in Javascript are used to store:',
+        options: ['Booleans', 'Strings','Numnbers','All of the above'],
+        answer: 'All of the above',
+    },
+    
+    {
+        numb: 2,
+        question: 'A timer needs to have which of the following methods to initate the countdown',
+        options: ['setInterval','addEventListener','appendChild','navigate'],
+        answer: 'setInterval',
+    },
+    {
+        numb: 3,
+        question: 'How can one access an element by the ID attribute from the html document?',
+        options: ['setAttribute','querySelectorAll','log','querySelector'],
+        answer: 'querySelector',
+    },
+    
+    {   
+        numb: 4,
+        question: 'The parameters of a function are enclosed in which of the following?',
+        options: ['curly brackets','quotes','parentheses','commas'],
+        answer: 'parentheses',
+    },
+    
+    {
+        numb: 5,
+        question: 'What type of event will listen for a click on a button',
+        options: ['preventDefault','stopPropagation','addEventListener','Console Log'],
+        answer: 'addEventListener',
+    },
+    
+    {   
+        numb: 6,
+        question: 'Which of the following is a commonly used data type?',
+        options: ['arrays','strings','events','values'],
+        answer: 'strings',
+    },
+    ];
